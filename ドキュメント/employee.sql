2ef7750f57ac58649632b91e5c7cef5b118d6a6c
takimoto <takimoto1>
2015-06-17 10:02:27 +0900

    ドキュメント追加

---
+++ ドキュメント/employee.sql
@@ -0,0 +1,26 @@
+DROP TABLE attendance_management.employee;
+
+CREATE TABLE attendance_management.employee (emp_no VARCHAR(4) PRIMARY KEY, emp_name VARCHAR(30) NOT NULL, emp_pass VARCHAR(15) NOT NULL);
+
+INSERT INTO attendance_management.employee VALUES ('M001','田中寿法','tanakats');
+
+INSERT INTO attendance_management.employee VALUES ('M002','牛木幸紀','ushikit');
+
+INSERT INTO attendance_management.employee VALUES ('E002','笹原大時','sasaharad');
+
+INSERT INTO attendance_management.employee VALUES ('E003','齋藤渉','saitow');
+
+INSERT INTO attendance_management.employee VALUES ('E005','岡本潤','okamotoj');
+
+INSERT INTO attendance_management.employee VALUES ('E006','田端亮','tabatar');
+INSERT INTO attendance_management.employee VALUES ('E007','盛竜也','morit');
+
+INSERT INTO attendance_management.employee VALUES ('E008','國小葉','kokusyoy');
+
+INSERT INTO attendance_management.employee VALUES ('E009','西尾知美','kokusyoy');
+
+INSERT INTO attendance_management.employee VALUES ('E010','滝本博昭','tanakats');
+
+INSERT INTO attendance_management.employee VALUES ('E011','大滝昇平','ohtakis');
+
+commit;
---
+++ ドキュメント/work_info.sql
@@ -0,0 +1,7 @@
+DROP TABLE attendance_management.work_info;
+
+CREATE TABLE attendance_management.work_info (info_no BIGINT AUTO_INCREMENT PRIMARY KEY, emp_no VARCHAR(4) NOT NULL, date DATE NOT NULL, work_year SMALLINT NOT NULL, work_month TINYINT NOT NULL, work_day TINYINT NOT NULL, work_start VARCHAR(10), work_last VARCHAR(10), real_start VARCHAR(10), real_last VARCHAR(10));
+
+INSERT INTO attendance_management.work_info (emp_no, date, work_year, work_month, work_day, work_start, real_start) VALUES ('E010', cast( now() as date), 2000, 1, 1 , '12:00', '11:59');
+
+commit;
